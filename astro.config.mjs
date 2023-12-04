82111fd65dca78819ce3050c7ac5065b7109d31a
Yash <78804783+yashd-dev@users.noreply.github.com>
2023-12-05 02:19:45 +0530

    Some What UI Done

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-
+import { astroImageTools } from "astro-imagetools";
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  integrations: [tailwind(), react(), astroImageTools],
 });
