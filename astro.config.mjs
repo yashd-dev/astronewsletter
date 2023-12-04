6d007d90284bbb64de99f0cf38a34ce864558cb7
Yash <78804783+yashd-dev@users.noreply.github.com>
2023-12-05 02:26:27 +0530

    Added PartyTown

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,9 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import { astroImageTools } from "astro-imagetools";
+import partytown from "@astrojs/partytown";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), astroImageTools],
+  integrations: [tailwind(), react(), astroImageTools, partytown()]
 });
