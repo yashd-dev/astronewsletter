c20cfb913fcc95dfa8bf4809d107e32581656195
Yash <78804783+yashd-dev@users.noreply.github.com>
2023-12-07 20:53:55 +0530

    Optimizing For Speed

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,21 +3,18 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import { astroImageTools } from "astro-imagetools";
 import partytown from "@astrojs/partytown";
-
 import sanity from "astro-sanity";
+import sitemap from "@astrojs/sitemap";
+
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind(),
-    react(),
-    astroImageTools,
-    partytown(),
-    sanity({
-      projectId: "cql3n56o",
-      dataset: "production",
-      apiVersion: "2021-03-25",
-      useCdn: true,
-    }),
-  ],
+  site: "https://newsletter.yashd.tech",
+  integrations: [tailwind(), react(), astroImageTools, partytown(), sanity({
+    projectId: "cql3n56o",
+    dataset: "production",
+    apiVersion: "2021-03-25",
+    useCdn: true
+  }), sitemap(), compress()]
 });
